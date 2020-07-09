d459840f00daf3a00d88eed7f6ad4d490992d7f1
scr3amind <screamindarko@gmail.com>
2020-07-09 12:03:38 -0500

    Obteniendo detalles del cast

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/src/models/actores_model.dart';
 import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
 
 
 class PeliculaDetalle extends StatelessWidget {
@@ -20,11 +22,7 @@ class PeliculaDetalle extends StatelessWidget {
                 SizedBox(height: 10.0,),
                 _posterTitulo(context, pelicula),
                 _descripcion(context, pelicula),
-                _descripcion(context, pelicula),
-                _descripcion(context, pelicula),
-                _descripcion(context, pelicula),
-                _descripcion(context, pelicula),
-                _descripcion(context, pelicula),
+                _crearCasting(pelicula),
               ]
             ),
           ),
@@ -101,5 +99,67 @@ class PeliculaDetalle extends StatelessWidget {
       ),
     );
 
+  }
+
+  Widget _crearCasting(Pelicula pelicula) {
+    final peliProvider = new PeliculasProvider();
+    return FutureBuilder(
+      future: peliProvider.getCast(pelicula.id.toString()),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+
+        if(snapshot.hasData) {
+          return _crearActoresPageView(snapshot.data);
+        }
+        else{
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+          
+        }
+
+      },
+    );
+  }
+
+  Widget _crearActoresPageView(List<Actor> actores) {
+
+    return SizedBox(
+      height: 200.0,
+      child: PageView.builder(
+        pageSnapping: false,
+        controller: PageController(
+          viewportFraction: 0.3,
+          initialPage: 1,        
+        ),
+        itemCount: actores.length,
+        itemBuilder: (context,i) {
+          return _actorTarjeta(actores[i]);
+        },
+
+      ),
+    );
+
+  }
+
+  Widget _actorTarjeta(Actor actor) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/img/no-image.jpg'), 
+              image: NetworkImage(actor.getFoto()),
+              height: 150.0,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Text(
+            actor.name,
+            overflow: TextOverflow.ellipsis,
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:peliculas/src/models/actores_model.dart';
 import 'package:peliculas/src/models/pelicula_model.dart';
 
 class PeliculasProvider {
@@ -64,5 +65,18 @@ class PeliculasProvider {
     return resp;
   }
 
+
+  Future<List<Actor>> getCast(String peliId) async {
+    final url = Uri.https(_url, '3/movie/$peliId/credits',{
+      'api_key'   : _apikey,
+      'language'  : _languaje
+    });
+
+    final resp = await http.get(url);
+    final decodedData = json.decode(resp.body);
+
+    final cast = new Cast.fromJsonList(decodedData['cast']);
+    return cast.actores;
+  }
 
 }
